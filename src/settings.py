40c0f8dc45a4b5d6babe8c21380aafecb3db1e66
Azamat Galimzhanov <azamat@galimzhanov.com>
2020-03-11 11:32:27 +0600

    Add token auth

--- src/settings.py
+++ src/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework.authtoken',
 
     'src.apps.tasks',
     'src.apps.users'
--- src/urls.py
+++ src/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from rest_framework.authtoken import views
+
 
 from src.apps.tasks.api import urlpatterns as tasks_api
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/auth/', views.obtain_auth_token)
 ] + tasks_api
