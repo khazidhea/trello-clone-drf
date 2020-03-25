925e8c11457e240f64138336e9dc748f741f172d
Azamat Galimzhanov <azamat@galimzhanov.com>
2020-03-25 14:54:44 +0600

    Add mypy

--- src/settings.py
+++ src/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from typing import List
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = 'nkumge6&4r2nxyah&$6i4$ow$se&kbfm8#m+4y$ikw8^ji$jwc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: List[str] = []
 
 
 # Application definition
